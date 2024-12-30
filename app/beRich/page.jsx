c75d7ba2c605cb5fec66f714af3d3cce5579da80
Yazan <abo.ayash.yazan@gmail.com>
2024-12-30 12:27:52 +0100

    loadingscreen page added

--- app/beRich/page.jsx
+++ app/beRich/page.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+'use client';
+import { useState, useEffect } from "react";
 import Intro from './components/Intro';
+import LoadingScreen from "@components/LoadingScreen";
+
+
 
 
 
 const beRichHome = () => {
+    const [isLoading, setIsLoading] = useState(true);
+  
+    useEffect(() => {
+      const timer = setTimeout(() => {
+        setIsLoading(false);
+      }, 2200);
+      return () => clearTimeout(timer);
+    }, []);
  
   return (
+    <>
+      {isLoading && (
+        <div className="fixed top-0 left-0 w-full h-full z-50 bg-black">
+          <LoadingScreen onComplete={() => setIsLoading(false)} 
+          text="beRich.Hub"/>
+      </div>
+      )}
       <section className='w-full'>  
         <div className="relative left-0
          w-full">
@@ -15,6 +34,7 @@ const beRichHome = () => {
         </div>
         <Intro />
       </section>
+    </>
     
   );
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,30 @@
+'use client';
+
+import { useState, useEffect } from "react";
+import LoadingScreen from "@components/LoadingScreen";
 import Hero from "@components/main/Hero";
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 2200);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <main className="relative flex justify-center items-center h-screen">
-      <Hero />
-    </main>
+    <>
+      {isLoading && (
+        <div className="fixed top-0 left-0 w-full h-full z-50 bg-black">
+          <LoadingScreen onComplete={() => setIsLoading(false)}
+          text="ColdByDefault" />
+        </div>
+      )}
+      <main className="relative flex justify-center items-center h-screen">
+        <Hero />
+      </main>
+    </>
   );
 }
