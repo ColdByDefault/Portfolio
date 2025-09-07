d7898cfaaca651127c0652f8598f0a1187c6ffd9
Yazan <abo.ayash.yazan@gmail.com>
2025-09-07 15:01:39 +0200

    feat: Update Navbar to use translation for media label and add Quick Actions export

--- components/nav/Navbar.tsx
+++ components/nav/Navbar.tsx
@@ -59,7 +59,7 @@ export default function Navbar() {
       icon: BookOpenCheck,
     },
     {
-      name: "Media",
+      name: t("media"),
       href: "/media",
       icon: SquareLibrary,
     },
