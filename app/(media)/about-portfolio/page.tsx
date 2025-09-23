06157a7227c6262552964a3c86696b520d1b7700
ColdByDefault <abo.ayash.yazan@gmail.com>
2025-09-23 13:51:50 +0200

    refactor(about-portfolio): remove unused components and logic from AboutPortfolioPage

--- app/(media)/about-portfolio/page.tsx
+++ app/(media)/about-portfolio/page.tsx
@@ -3,14 +3,13 @@
  * @copyright 2025 ColdByDefault. All Rights Reserved.
  */
 
-import { AboutPortfolioPage } from "@/components/aboutPortoPage";
+
 import React from "react";
 
 export default function AboutPortfolioPageRoute() {
   return (
     <div className="min-h-screen relative">
       <div className="relative z-10">
-        <AboutPortfolioPage />
       </div>
     </div>
   );
