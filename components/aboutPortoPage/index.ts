a6f4a80f77c8abe682f194240786edefe3611743
ColdByDefault <abo.ayash.yazan@gmail.com>
2025-09-23 13:53:55 +0200

    refactor(about-portfolio): remove unused exports and types from AboutPortfolio components

--- components/aboutPortoPage/index.ts
+++ components/aboutPortoPage/index.ts
@@ -4,17 +4,4 @@
  * @copyright 2025 ColdByDefault. All Rights Reserved.
  */
 
-export { default as AboutPortfolioPage } from "./AboutPortfolioPage";
-export { HeroSection } from "./HeroSection";
-export { NavigationTabs } from "./NavigationTabs";
-export { OverviewSection } from "./OverviewSection";
-export { TechnicalStackSection } from "./TechnicalStackSection";
-export { FeaturesSection } from "./FeaturesSection";
-export { PerformanceSection } from "./PerformanceSection";
-export { SecuritySection } from "./SecuritySection";
-export { AIIntegrationSection } from "./AIIntegrationSection";
-export { useAboutPortfolioNavigation } from "./AboutPortfolioPage.logic";
-export type {
-  NavigationSection,
-  AboutPortfolioNavigation,
-} from "./AboutPortfolioPage.logic";
+
