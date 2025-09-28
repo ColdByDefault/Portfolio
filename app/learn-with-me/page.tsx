672d1d2ca6417d1d7cc66cb1a548e2fca2c5f5c7
ColdByDefault <abo.ayash.yazan@gmail.com>
2025-09-28 10:14:47 +0200

    refactor: Remove sidebar component and related layout for ResourcesPage

--- app/learn-with-me/page.tsx
+++ app/learn-with-me/page.tsx
@@ -3,16 +3,13 @@
  * @copyright 2025 ColdByDefault. All Rights Reserved.
  */
 
-"use client";
-
-import { ResourceSidebar } from "@/components/learn-with-me/sidebar";
 import { TextViewer } from "@/components/learn-with-me/text-viewer";
+
 
 export default function ResourcesPage() {
   return (
-    <div className="flex h-screen bg-background">
-      <ResourceSidebar className="w-64 flex-shrink-0" />
-      <TextViewer />
-    </div>
+      <div className="flex h-screen bg-background">
+        <TextViewer />
+      </div>
   );
 }
