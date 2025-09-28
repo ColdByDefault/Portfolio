a9c64d88dc7095f38e571040f6fa74d680e08196
ColdByDefault <abo.ayash.yazan@gmail.com>
2025-09-28 17:01:25 +0200

    refactor: Simplify imports in Home component and remove loading state management

--- components/visuals/index.ts
+++ components/visuals/index.ts
@@ -7,5 +7,6 @@
 export { default as LoadingScreen } from "./LoadingScreen";
 export { default as LoadingSkeleton } from "./LoadingSkeleton";
 export { default as CentralizedLoading } from "./CentralizedLoading";
+export { default as ClientBackground } from "./ClientBackground";
 export { Background } from "./motion-background";
 export { ImageZoomDialog } from "./image-zoom-dialog";
