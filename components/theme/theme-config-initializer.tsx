18aca5ab2ccb838fc93a91d2c7b73db95f6fa354
ColdByDefault <abo.ayash.yazan@gmail.com>
2025-12-29 19:27:21 +0100

    fix: restore theme config initialization for development stability

--- components/theme/theme-config-initializer.tsx
+++ components/theme/theme-config-initializer.tsx
@@ -8,22 +8,22 @@
 // and NEXT_PUBLIC_THEME_VALIDATION_HASH env variables are missing in development mode.
 // Uncomment if you have those env variables set properly.
 //
-// import { initializeThemeConfig } from "./theme-config-validator";
-// import { useEffect } from "react";
+import { initializeThemeConfig } from "./theme-config-validator";
+import { useEffect } from "react";
 //
-// export function ThemeConfigInitializer() {
-//   useEffect(() => {
-//     initializeThemeConfig();
-//   }, []);
-//   return null;
-// }
+export function ThemeConfigInitializer() {
+  useEffect(() => {
+    initializeThemeConfig();
+  }, []);
+  return null;
+}
 
 /**
  * NEW CONFIG - Safe version that doesn't require env variables
  * Client-side CSS performance initialization component
  */
-export function ThemeConfigInitializer() {
+/* export function ThemeConfigInitializer() {
   // Theme config initialization disabled for development stability
   // Enable the old config above if you have the required env variables
   return null;
-}
+} */
