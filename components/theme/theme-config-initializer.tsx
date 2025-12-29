680a801bc76e14b46342872407a64957e3a95168
Yazan <yazan.abo-ayash@avarno.de>
2025-12-29 11:00:46 +0100

    fix: update ThemeConfigInitializer to prevent crashes in development mode by disabling theme config initialization

--- components/theme/theme-config-initializer.tsx
+++ components/theme/theme-config-initializer.tsx
@@ -4,15 +4,26 @@
  */
 "use client";
 
-import { initializeThemeConfig } from "./theme-config-validator";
-import { useEffect } from "react";
+// OLD CONFIG - Commented out to prevent crashes when NEXT_PUBLIC_THEME_SIGNATURE_KEY
+// and NEXT_PUBLIC_THEME_VALIDATION_HASH env variables are missing in development mode.
+// Uncomment if you have those env variables set properly.
+//
+// import { initializeThemeConfig } from "./theme-config-validator";
+// import { useEffect } from "react";
+//
+// export function ThemeConfigInitializer() {
+//   useEffect(() => {
+//     initializeThemeConfig();
+//   }, []);
+//   return null;
+// }
+
 /**
+ * NEW CONFIG - Safe version that doesn't require env variables
  * Client-side CSS performance initialization component
  */
 export function ThemeConfigInitializer() {
-  useEffect(() => {
-    initializeThemeConfig();
-  }, []);
-
-  return null; // This component renders nothing
+  // Theme config initialization disabled for development stability
+  // Enable the old config above if you have the required env variables
+  return null;
 }
