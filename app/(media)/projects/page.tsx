0be5dd54c14773e3aadb539e563238f5f3cb019b
ColdByDefault <abo.ayash.yazan@gmail.com>
2026-02-17 16:52:14 +0100

    refactor: clean up About and QuickActions components, update layout and add GitHub showcase

--- app/(media)/projects/page.tsx
+++ app/(media)/projects/page.tsx
@@ -4,6 +4,7 @@
 */
 
 'use client';
+import GitHubShowcase from "@/components/github/GitHubShowcase";
 import {ProjectsShowcase} from "@/components/projects";
 
 
@@ -12,6 +13,12 @@ export default function LibraryPage() {
   return (
     <div className="container mx-auto px-4 py-8">
       <ProjectsShowcase className="py-12 px-4 sm:px-6 lg:px-8" />
+              {/* GitHub Showcase Section */}
+        <section className="pb-20 px-4 lg:px-8" id="github">
+          <div className="max-w-6xl mx-auto">
+            <GitHubShowcase />
+          </div>
+        </section>
     </div>
   );
 }
