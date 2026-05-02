f3a80d63ea446dc108664e0d1fe6244b03b5df11
ColdByDefault <abo.ayash.yazan@gmail.com>
2026-05-02 09:35:34 +0200

    feat: remove unused components and hooks; clean up AboutPage and update exports

--- components/visuals/index.ts
+++ components/visuals/index.ts
@@ -1,10 +1,9 @@
 /**
  * @author ColdByDefault
  * @copyright  2026 ColdByDefault. All Rights Reserved.
-*/
+ */
 
 // Visual effects and loading components
-export { default as LoadingScreen } from "./LoadingScreen";
 export { default as LoadingSkeleton } from "./LoadingSkeleton";
 export { default as CentralizedLoading } from "./CentralizedLoading";
 export { Background } from "./motion-background";
